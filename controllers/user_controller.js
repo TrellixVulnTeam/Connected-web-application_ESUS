1b9b6e51b6103dc8cc78335345c7257a22c51212
ankita007-coder <85255403+ankita007-coder@users.noreply.github.com>
2022-05-27 13:37:10 +0530

    The sign up and sign in page creation started

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,17 @@
 module.exports.profile = function(req,res){
     res.end('<h1>user profile</h1>');
 }
+
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Connected | Sign Up"
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Connected | Sign In"
+    })
+}
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
 //use express router
 app.use('/', require('./routes'));
+//set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+//to interact with static pages
+app.use(express.static('./assets'));
 
 
 
