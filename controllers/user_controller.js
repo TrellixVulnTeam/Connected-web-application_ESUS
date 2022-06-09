21520bfdfa05410135b11dfe506547da1ca205d4
ankita007-coder <ankitabudhia42@gmail.com>
2022-06-09 16:37:32 +0530

    passport js authentication completed

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,51 +1,69 @@
 const User = require('../models/user');
-const user = require('../models/user');
 
 
-module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title: 'user profile'
+module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title: 'User Profile'
     })
 }
 
-//render the sign up page
-module.exports.signUp = function(req,res){
-    return res.render('user_sign_up',{
-        title: "Connected | Sign Up"
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
     })
 }
-//action to get the sign up data
-module.exports.create = function(req,res){
+
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
-    User.findOne({email: req.body.email}, function(err,user){
-        if(err){console.log('Error in finding user in signing up');
-    return;
-    }
-    if(!user){
-        User.create(req.body, function(err,user){
-            if(err){console.log('Error in creating user while signing up');return}
 
-            return res.redirect('/users/sign-in');
-        })
-    }else{
-        return res.redirect('back');
-    }
-    })
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    });
 }
 
 
-
-
-
-//render the sign in page
-module.exports.signIn = function(req,res){
-    return res.render('user_sign_in',{
-        title: "Connected | Sign In"
-    })
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    return res.redirect('/');
 }
-//sign in and create a session for the user
-module.exports.createSession = function(req,res){
-    //TODO later
+
+
+module.exports.destroySession = function(req, res){
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
 }
