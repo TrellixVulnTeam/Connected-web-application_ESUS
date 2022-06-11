596d79a879c7bf9b150da72a5b03f928cf96344f
ankita007-coder <ankitabudhia42@gmail.com>
2022-06-11 19:46:23 +0530

    The feature for commenting is added

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-console.log('Router loaded');
 
-router.get('/',homeController.home);
+console.log('router loaded');
 
-router.use('/users',require('./users'));
-router.use('/post',require('./post'));
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
 
 
 module.exports = router;
